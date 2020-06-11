e79283827e48c615a13704a830496519a3073e0d
钱旭 <1464238196@qq.com>
2020-06-11 19:46:19 +0800

    尝试axios 天坑的query

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import qs from 'qs'
 
 Vue.config.productionTip = false
+
+var axiosInstance = axios.create({
+  baseURL: 'http://localhost:8081'
+})
+
+Vue.prototype.axiosInstance = axiosInstance
+Vue.prototype.axios = axios
+Vue.prototype.qs = qs
 
 /* eslint-disable no-new */
 new Vue({
