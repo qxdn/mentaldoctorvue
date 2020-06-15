803f4308b3c7af6f08e31d9c1be5589b7a69c0ce
钱旭 <1464238196@qq.com>
2020-06-15 21:22:40 +0800

    请求列表

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <el-button type="primary" @click="getPage">测试</el-button>
+    <p v-for="(item,i) in postList" :key="i">{{item}}</p>
+    <el-pagination
+      background
+      layout="total, prev, pager, next, jumper"
+      @current-change="handleCurrentChange"
+      :page-size="pageSize"
+      :current-page="currentPage"
+      :total="total"
+      >
+    </el-pagination>
   </div>
 </template>
 
@@ -8,14 +17,34 @@
 export default {
   name: 'PostList',
   data () {
-    return {}
+    return {
+      pageSize: 10,
+      total: 1,
+      currentPage: 1,
+      postList: ''
+    }
   },
   methods: {
-    getPage () {
-      this.getRequest('/post/', null).then((response) => {
-        console.log(response)
+    handleCurrentChange (val) {
+      // 拼接url
+      let url = '/post/?page=' + (val - 1)
+      this.getRequest(url, null).then((response) => {
+        this.handleRespone(response)
       })
+    },
+    initPage () {
+      this.getRequest('/post/', null).then((response) => {
+        this.handleRespone(response)
+      })
+    },
+    handleRespone (response) {
+      this.total = response.data.totalElements
+      this.postList = response.data.content
+      this.pageSize = response.data.size
     }
+  },
+  created: function () {
+    this.initPage()
   }
 }
 </script>
